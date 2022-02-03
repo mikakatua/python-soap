c873688334f730a235d5aff59fde66c3cf29df59
Paco Martinez <paco.martinez@ing.com>
2022-02-03 07:54:35 +0100

    Add XML output

--- capitals.py
+++ capitals.py
@@ -1,3 +1,4 @@
+
 #import zeep
 #import xml.etree.ElementTree as ET
 from zeep.plugins import HistoryPlugin
@@ -6,16 +7,22 @@ from lxml import etree
 
 #wsdl_url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL"
 wsdl_url = 'CountryInfoService.wsdl'
+
 history = HistoryPlugin()
 client = Client(wsdl_url, plugins=[history])
 client.service.ListOfCountryNamesByCode()
-your_pretty_xml = etree.tostring(history.last_received["envelope"], encoding="unicode", pretty_print=True)
-#print(your_pretty_xml)
+# Print the SOAP response
+print(etree.tostring(history.last_received["envelope"], encoding="unicode", pretty_print=True))
 
 tree = etree.parse('countries.xml')
 root = tree.getroot()
-for item in root.findall('tCountryCodeAndName'):
+for index, item in enumerate(root.findall('tCountryCodeAndName')):
   code = item.find('sISOCode').text
   country = item.find('sName').text
+  request = client.create_message(client.service, 'CapitalCity', sCountryISOCode=code)
+  # Print the SOAP request
+  print(etree.tostring(request, encoding="unicode", pretty_print=True))
   capital = client.service.CapitalCity(sCountryISOCode=code)
   print(f"Capital of {country} is {capital}")
+  if index == 1:
+    break
